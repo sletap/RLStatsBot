0837a2d4214c968864a6bfaebf53b9e7d2be031d
sletap <34560634+sletap@users.noreply.github.com>
2018-08-29 14:56:49 -0500

    !compare feature, index finder added
    Two players can now be compared using !compare. The function responsible is writeStatsComparison. Index finder was placed in a function to reduce redundancy. This function finds where a player is located in the fullDataSet list. the original writeStats function was renamed to writeStatsSingle for a single player.

--- OctaneGGStats.py
+++ OctaneGGStats.py
@@ -1,9 +1,9 @@
 # stats from Octane.gg, static implementation
 import RLDataSet
+from decimal import Decimal
 
 # Parses data using Beautiful Soup 4 and return a list with all needed information
 def learnStatistics():
-
 	fullDataSet = RLDataSet.fullDataSet # this is where all the information will be stored
 	return fullDataSet
 
@@ -15,16 +15,19 @@ def makePlayersList(fullDataSet):
 
 	return players
 
-# returns the data given the name of a player, which is the query
-def writeStats(query, players, fullDataSet):
-	# finds where the player is located in the data set
+def indexFinder(query, players):
 	index = 0
 	for person in players:
 		if person.lower() == query.lower():
 			break	
 		else:
 			index = index + 1
+	return index
 
+# returns the data given the name of a player, which is the query
+def writeStatsSingle(query, players, fullDataSet):
+	# finds where the player is located in the data set
+	index = indexFinder(query, players)
 	underscoredString = query.replace(" ", "_")
 
 	# strings for if player does not exist or if a player does exist
@@ -34,7 +37,7 @@ def writeStats(query, players, fullDataSet):
 		return reply
 	else:
 		reply = "# Statistics for [" + query + ":](https://octane.gg/player/" + underscoredString
-		reply = reply + ")\n\n" + query + "|Stats|" + "\n"
+		reply = reply + ")\n\n" + "Stats|" + query + "|\n"
 		reply = reply + ":---|---:|" + "\n"
 		reply = reply + "Games Played|" + fullDataSet[index][1] + "|\n"
 		reply = reply + "Win Percentage|" + fullDataSet[index][2] + "|\n"
@@ -46,11 +49,45 @@ def writeStats(query, players, fullDataSet):
 		reply = reply + "---" + "\n\n"
 		return reply
 
+def writeStatsComparison(queryOne, queryTwo, players, fullDataSet):
+	indexOne = indexFinder(queryOne, players)
+	underscoredStringOne = queryOne.replace(" ", "_")
+
+	indexTwo = indexFinder(queryTwo, players)
+	underscoredStringTwo = queryTwo.replace(" ", "_")
+
+	if indexOne >= len(fullDataSet) or indexTwo >= len(fullDataSet):
+		reply = "Invalid usage, are either of your players misspelled or have you formatted incorrectly? \n\n"
+		reply = reply + "---" + "\n\n"
+		return reply
+	else:
+		reply = "# Statistics for [" + queryOne + "](https://octane.gg/player/" + underscoredStringOne + ")"
+		reply = reply +" and [" + queryTwo + ":](https://octane.gg/player/" + underscoredStringTwo
+		reply = reply + ")\n\n" + "Stats|" + queryOne + "|" + queryTwo + "|Differential|\n"
+		reply = reply + ":---|:---:|:---:|---:|" + "\n"
+		reply = reply + "Games Played|" + fullDataSet[indexOne][1] + "|" + fullDataSet[indexTwo][1] + "|"
+		reply = reply + str(Decimal(fullDataSet[indexOne][1]) - Decimal(fullDataSet[indexTwo][1])) + "|\n"
+		reply = reply + "Win Percentage|" + fullDataSet[indexOne][2] + "|" + fullDataSet[indexTwo][2] + "|"
+		reply = reply + str(Decimal(fullDataSet[indexOne][2].strip('%')) - Decimal(fullDataSet[indexTwo][2].strip('%'))) + "%	|\n"
+		reply = reply + "Average Score Per Game|" + fullDataSet[indexOne][3] + "|" + fullDataSet[indexTwo][3] + "|"
+		reply = reply + str(Decimal(fullDataSet[indexOne][3]) - Decimal(fullDataSet[indexTwo][3])) + "|\n"
+		reply = reply + "Average Goals Per Game|" + fullDataSet[indexOne][4] + "|" + fullDataSet[indexTwo][4] + "|"
+		reply = reply + str(Decimal(fullDataSet[indexOne][4]) - Decimal(fullDataSet[indexTwo][4])) + "|\n"
+		reply = reply + "Average Assists Per Game|" + fullDataSet[indexOne][5] + "|" + fullDataSet[indexTwo][5] + "|"		
+		reply = reply + str(Decimal(fullDataSet[indexOne][5]) - Decimal(fullDataSet[indexTwo][5])) + "|\n"
+		reply = reply + "Average Saves Per Game|" + fullDataSet[indexOne][6] + "|" + fullDataSet[indexTwo][6] + "|"
+		reply = reply + str(Decimal(fullDataSet[indexOne][6]) - Decimal(fullDataSet[indexTwo][6])) + "|\n"
+		reply = reply + "Average Shots Per Game|" + fullDataSet[indexOne][7] + "|" + fullDataSet[indexTwo][7] + "|"
+		reply = reply + str(Decimal(fullDataSet[indexOne][7]) - Decimal(fullDataSet[indexTwo][7])) + "|\n"
+		reply = reply + "---" + "\n\n"
+		return reply
+
 # example run for testing outside of a reddit instance
 def main():
 	fullDataSet = learnStatistics()
 	players = makePlayersList(fullDataSet)
-	print(writeStats("Scrub Killa", players, fullDataSet))
+	print(writeStatsComparison("Fireburner", "GarrettG", players, fullDataSet))
+	# print(writeStatsSingle("Scrub Killa", players, fullDataSet))
 
 if __name__ == "__main__":
 	main()
