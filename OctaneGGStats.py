64d716911131663c085ee3fc50c0eb30f6de0faa
Sahil Patel <34560634+sletap@users.noreply.github.com>
2018-08-31 18:08:59 -0500

    Added Percentile Feature
    Shows what percentile a player is within each category

--- OctaneGGStats.py
+++ OctaneGGStats.py
@@ -1,20 +1,39 @@
-# stats from Octane.gg, static implementation
+# stats from Octane.gg
 import RLDataSet
-from decimal import Decimal
+import json
+import decimal
+from scipy import stats
 
-# Parses data using Beautiful Soup 4 and return a list with all needed information
 def learnStatistics():
-	fullDataSet = RLDataSet.fullDataSet # this is where all the information will be stored
+	fullDataSet = RLDataSet.fullDataSet
 	return fullDataSet
 
+def listStringToFloat(numList):
+	if "%" in numList[0]:
+		index = 0
+		for item in numList:
+			numList[index] = item.strip("%")
+			index += 1
+	numList = list(map(float, numList))
+	return numList
+
+def findPercentile(fullDataSet, query, statIndex):
+	players = makePlayersList(fullDataSet, 0) # get players list
+	index = indexFinder(query,players) # get player index
+	gamesPlayed = makePlayersList(fullDataSet, statIndex) # gets required stat in list form
+	gamesPlayed = listStringToFloat(gamesPlayed) # converts list from string to float
+	percentile = stats.percentileofscore(gamesPlayed,float(fullDataSet[index][statIndex].strip("%")),kind="weak")
+	percentile = str(round(percentile,2))
+	return percentile
+
 # creates a list of only players so we can search through them when a user calls for a player
-def makePlayersList(fullDataSet):
+def makePlayersList(fullDataSet, index):
 	players = [] # this is where we will index players
 	for player in fullDataSet:
-		players.append(player[0])
-
+		players.append(player[index])
 	return players
 
+# finds the index of where a player's information is located
 def indexFinder(query, players):
 	index = 0
 	for person in players:
@@ -37,15 +56,15 @@ def writeStatsSingle(query, players, fullDataSet):
 		return reply
 	else:
 		reply = "# Statistics for [" + query + ":](https://octane.gg/player/" + underscoredString
-		reply = reply + ")\n\n" + "Stats|" + query + "|\n"
-		reply = reply + ":---|---:|" + "\n"
-		reply = reply + "Games Played|" + fullDataSet[index][1] + "|\n"
-		reply = reply + "Win Percentage|" + fullDataSet[index][2] + "|\n"
-		reply = reply + "Average Score Per Game|" + fullDataSet[index][3] + "|\n"
-		reply = reply + "Average Goals Per Game|" + fullDataSet[index][4] + "|\n"
-		reply = reply + "Average Assists Per Game|" + fullDataSet[index][5] + "|\n"
-		reply = reply + "Average Saves Per Game|" + fullDataSet[index][6] + "|\n"
-		reply = reply + "Average Shots Per Game|" + fullDataSet[index][7] + "|\n"
+		reply = reply + ")\n\n" + "Stats|" + query + "|Percentile|\n"
+		reply = reply + ":---|---:|---:|" + "\n"
+		reply = reply + "Games Played|" + fullDataSet[index][1] + "|" + findPercentile(fullDataSet,query,1) + "|\n"
+		reply = reply + "Win Percentage|" + fullDataSet[index][2] + "|" + findPercentile(fullDataSet,query,2) + "|\n"
+		reply = reply + "Average Score Per Game|" + fullDataSet[index][3] + "|" + findPercentile(fullDataSet,query,3) + "|\n"
+		reply = reply + "Average Goals Per Game|" + fullDataSet[index][4] + "|" + findPercentile(fullDataSet,query,4) + "|\n"
+		reply = reply + "Average Assists Per Game|" + fullDataSet[index][5] + "|" + findPercentile(fullDataSet,query,5) + "|\n"
+		reply = reply + "Average Saves Per Game|" + fullDataSet[index][6] + "|" + findPercentile(fullDataSet,query,6) + "|\n"
+		reply = reply + "Average Shots Per Game|" + fullDataSet[index][7] + "|" + findPercentile(fullDataSet,query,7) + "|\n"
 		reply = reply + "---" + "\n\n"
 		return reply
 
@@ -85,9 +104,9 @@ def writeStatsComparison(queryOne, queryTwo, players, fullDataSet):
 # example run for testing outside of a reddit instance
 def main():
 	fullDataSet = learnStatistics()
-	players = makePlayersList(fullDataSet)
-	print(writeStatsComparison("Fireburner", "GarrettG", players, fullDataSet))
-	# print(writeStatsSingle("Scrub Killa", players, fullDataSet))
+	players = makePlayersList(fullDataSet, 0)
+	# print(writeStatsComparison("Scrub Killa", "GarrettG", players, fullDataSet))
+	print(writeStatsSingle(input("Enter Player Name: "), players, fullDataSet))
 
 if __name__ == "__main__":
 	main()
